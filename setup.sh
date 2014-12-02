8a81dd953f7378f03a6073b68bf0b6e5ec95a7dd
Samuel Reed <samuel.trace.reed@gmail.com>
2014-12-02 12:54:35 +0100

    Fixup setup.sh

--- setup.sh
+++ setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-# For getting a new server up and running
-# Must be run with sudo
+# Must be run with sudo on most machine, where python packages can only
+# be installed with root.
 if [ "$(id -u)" != "0" ]; then
   echo "This script must be run as root (sudo bash setup.sh)" 1>&2
   exit 1
 fi
 
-echo "This script gets you set up with bitmex-btchip-signer."
+echo "This script gets you set up with btchip-signing-tools."
 
 pip install cython
 
@@ -25,5 +25,5 @@ python setup.py develop
 
 if [ ! -f settings.py ]; then
   cp settings.py.example settings.py
-  echo "Please example settings.py and fit to your environment."
+  echo "Please edit settings.py and fit to your environment."
 fi

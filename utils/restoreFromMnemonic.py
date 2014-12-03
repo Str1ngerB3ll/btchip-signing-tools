cfa7f0113a7001fe9eccfc8dc933aac3cf4ea9ec
Samuel Reed <samuel.trace.reed@gmail.com>
2014-12-03 10:12:34 +0100

    Rework setup scripts.

--- utils/restoreFromMnemonic.py
+++ utils/restoreFromMnemonic.py
@@ -1,33 +1,19 @@
-import mnemonic, binascii, getpass, sys
-from btchip.btchip import *
-from btchip.btchipUtils import *
+import mnemonic, binascii, getpass, sys, click
+from setupDongle import setup
 
-isTestnet = len(sys.argv) > 1 and sys.argv[1] == '--testnet'
+@click.command()
+@click.option('--testnet', default=False, is_flag=True, help="If set, will set up the dongle in testnet mode." )
+def main(testnet):
+  print "This script is for restoring a new BTChip from a mnemonic seed."
 
-M = mnemonic.Mnemonic("english")
+  M = mnemonic.Mnemonic("english")
+  wordlist = raw_input("Backup Mnemonic: ")
+  passphrase = getpass.getpass("Backup Passphrase (Press enter if blank):")
+  seed = binascii.hexlify(M.to_seed(wordlist, passphrase))
+  print 'mnemonic : %s (%d words)' % (wordlist, len(wordlist.split(' ')))
+  print 'seed     : %s (%d bits)\n' % (seed, len(seed) * 4)
 
-print "This script is for restoring a new BTChip from a mnemonic seed."
+  setup(bytearray(seed.decode('hex')), testnet)
 
-wordlist = raw_input("Backup Mnemonic: ")
-passphrase = getpass.getpass("Backup Passphrase (Press enter if blank):")
-seed = binascii.hexlify(M.to_seed(wordlist, passphrase))
-print 'mnemonic : %s (%d words)' % (wordlist, len(wordlist.split(' ')))
-print 'seed     : %s (%d bits)\n' % (seed, len(seed) * 4)
-SEED = bytearray(seed.decode('hex'))
-
-# Setup
-PIN = getpass.getpass("New PIN: ")
-dongle = getDongle(False)
-app = btchip(dongle)
-try:
-  if isTestnet:
-    app.setup(btchip.OPERATION_MODE_RELAXED_WALLET, btchip.FEATURE_RFC6979|btchip.FEATURE_NO_2FA_P2SH, 111, 196, PIN, None, btchip.QWERTY_KEYMAP, SEED)
-  else:
-    app.setup(btchip.OPERATION_MODE_RELAXED_WALLET, btchip.FEATURE_RFC6979|btchip.FEATURE_NO_2FA_P2SH, 0, 5, PIN, None, btchip.QWERTY_KEYMAP, SEED)
-except BTChipException as e:
-  if e.sw == 0x6985:
-    print "ERROR: Chip is already set up. If you want to set it up with a new seed, run resetChip.py first."
-  exit(1)
-  
-# Authenticate to stop key from emitting the seed anymore
-app.verifyPin(PIN)
+if __name__ == "__main__":
+  main()
--- utils/setupDongle.py
+++ utils/setupDongle.py
@@ -1,25 +1,39 @@
 from btchip.btchip import *
 from btchip.btchipUtils import *
-import getpass
+import getpass, click
 
-print "This script is for setting up a new BTChip."
+@click.command()
+@click.option('--testnet', default=False, is_flag=True, help="If set, will set up the dongle in testnet mode." )
+def main(testnet):
+  print "This script is for setting up a new BTChip."
 
-SEED = bytearray(raw_input("Paste your seed: ").decode('hex'))
-PIN = getpass.getpass("New PIN: ")
-# SEED = bytearray("1762F9A3007DBC825D0DD9958B04880284C88A10C57CF569BB3DADF7B1027F2D".decode('hex'))
+  seed = bytearray(raw_input("Paste your seed: ").decode('hex'))
+  # This is the test seed
+  # seed = bytearray("1762F9A3007DBC825D0DD9958B04880284C88A10C57CF569BB3DADF7B1027F2D".decode('hex'))
 
-# Optional setup
-dongle = getDongle(False)
-app = btchip(dongle)
-try:
-  # Testnet
-  app.setup(btchip.OPERATION_MODE_RELAXED_WALLET, btchip.FEATURE_RFC6979|btchip.FEATURE_NO_2FA_P2SH, 111, 196, PIN, None, btchip.QWERTY_KEYMAP, SEED)
-  # Mainnet
-  # app.setup(btchip.OPERATION_MODE_RELAXED_WALLET, btchip.FEATURE_RFC6979|btchip.FEATURE_NO_2FA_P2SH, 0, 5, PIN, None, btchip.QWERTY_KEYMAP, SEED)
-except BTChipException as e:
-  if e.sw == 0x6985:
-    print "ERROR: Chip is already set up. If you want to set it up with a new seed, run resetChip.py first."
-  exit(1)
-  
-# Authenticate
-app.verifyPin(PIN)
+  setup(seed, testnet)
+
+# Exported
+def setup(seed, testnet=False):
+  network = "Testnet" if testnet else "Mainnet"
+  print "Chip will be used on network: " + network
+
+  pin = getpass.getpass("New PIN: ")
+
+  dongle = getDongle(False)
+  app = btchip(dongle)
+  try:
+    if testnet:
+      app.setup(btchip.OPERATION_MODE_RELAXED_WALLET, btchip.FEATURE_RFC6979|btchip.FEATURE_NO_2FA_P2SH, 111, 196, pin, None, btchip.QWERTY_KEYMAP, seed)
+    else:
+      app.setup(btchip.OPERATION_MODE_RELAXED_WALLET, btchip.FEATURE_RFC6979|btchip.FEATURE_NO_2FA_P2SH, 0, 5, pin, None, btchip.QWERTY_KEYMAP, seed)
+  except BTChipException as e:
+    if e.sw == 0x6985:
+      print "ERROR: Chip is already set up. If you want to set it up with a new seed, run resetChip.py first."
+    exit(1)
+    
+  # Authenticate to stop key from emitting the seed anymore
+  app.verifyPin(pin)
+
+if __name__ == "__main__":
+  main()

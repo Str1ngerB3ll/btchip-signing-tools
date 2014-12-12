a79f4a2a22b4ce7d1e47f3c943d0084aa2ecfb37
Samuel Reed <samuel.trace.reed@gmail.com>
2014-12-12 13:39:23 +0100

    Update armory decode script to output raw transaction in case you need to broadcast manually.

--- utils/armory/decodeSigCollect.py
+++ utils/armory/decodeSigCollect.py
@@ -1,6 +1,6 @@
 import os, sys, inspect
-# Add ./BitcoinArmory to path so it works as if we were importing it from the inside.
-cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"BitcoinArmory")))
+# Add ../../BitcoinArmory to path so it works as if we were importing it from the inside.
+cmd_subfolder = os.path.realpath(os.path.abspath(os.path.join(os.path.split(inspect.getfile( inspect.currentframe() ))[0],"../../BitcoinArmory")))
 if cmd_subfolder not in sys.path:
     sys.path.insert(0, cmd_subfolder)
 
@@ -11,6 +11,13 @@ import settings
 import pprint
 pp = pprint.PrettyPrinter(indent=2)
 
+# We've had SIGCOLLECTs fail to broadcaset from Armory. Recently we had one with multiple
+# signed inputs completely fail with a `string index out of range` error in Armory.
+#
+# This tool takes in a SIGCOLLECT block and prints transaction details and a raw transaction that can be
+# broadcast via bitcoind or any blockchain API like blockchain.info.
+#
+# You need to have the BitcoinArmory repo cloned in the root directory of the signer repo.
 def decodeSigCollect():
   print "Paste your TXSIGCOLLECT below and press <enter>:\n"
   stopAt = "================================================================"
@@ -27,7 +34,13 @@ def decodeSigCollect():
 
   pp.pprint(txJSON)
   print "\n"
+
+  print "\nTransaction summary:\n"
   tx.pprint()
+  tx.evaluateSigningStatus().pprint()
+
+  print "\nRaw Transaction:\n"
+  print binary_to_hex(tx.getSignedPyTx(doVerifySigs=False).serialize())
 
 
 if __name__ == "__main__":
